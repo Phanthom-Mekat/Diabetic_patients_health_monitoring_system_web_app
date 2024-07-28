a0b756ede8849f8048f033b9ace64e247b5b1889
Mustaqueem <131261010+MustaqueemAlam@users.noreply.github.com>
2024-07-28 17:10:10 +0600

    uuid npm package added in nodemodules of backend folder
    modal not working yet. insertion failed.

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-
+//const feedbackRoutes = require('./routes/feedbackRoutes');
+const inputdemographicsroute = require('./routes/inputdemographicsroute');
 const app = express();
 const port = 8000;
 
@@ -12,22 +12,36 @@ app.use(bodyParser.json());
 // Serve static files from the Frontend directory
 app.use(express.static(path.join(__dirname, '..', 'Frontend')));
 
-// Use feedback routes
-app.use('/feedback', feedbackRoutes);
+// Use  routes
+//app.use('/feedback', feedbackRoutes);
+app.use('/patient/submit', inputdemographicsroute);
+
 
 // Serve the HTML files based on the route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'Frontend', 'main', 'systemAdmin', 'feedback.html'));
-});
-
-app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'Frontend', 'main', 'login_signup', 'login.html'));
 });
+
+/* app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Frontend', 'main', 'systemAdmin', 'feedback.html'));
+}); */
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Add other routes to serve other HTML files similarly
 // app.get('/other', (req, res) => {
